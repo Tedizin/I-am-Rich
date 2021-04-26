086df277913e3afd26f38e701c56e45ab4101119
Henrique Silva de Carvalho <henrique.silvac@outlook.com>
2021-04-26 00:48:47 -0300

    Primeiro commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Henrique Silva on 23/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
